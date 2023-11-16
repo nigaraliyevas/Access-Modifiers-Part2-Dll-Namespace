a344dd55941e9626e72b1493a969621abc3b6692
nigaraliyevas <naliyeva965@gmail.com>
2023-11-16 22:06:46 +0400

    feat:Tasks finished with success but not sure about Gun task is right or not

---
+++ Access-Modifiers-Part2-Dll-Namespace/Models/Gun.cs
@@ -0,0 +1,75 @@
+﻿namespace Access_Modifiers_Part2_Dll_Namespace.Models
+{
+    internal class Gun
+    {
+        private string _shootMode;
+        private int _bulletNumber;
+        public string RefillGun;
+        public string ShooterMode
+        {
+            get
+            {
+                return _shootMode;
+            }
+            set
+            {
+                while (value!= "Single".ToLower() && value!= "Multi".ToLower())
+                {
+                    Console.WriteLine("Please enter right...");
+                    value= Console.ReadLine().ToLower();
+                }
+                _shootMode = value;
+            }
+        }
+        public int Bullet
+        {
+            get=> _bulletNumber;  //Does it work like arrow func in JS ?
+            set
+            {
+                int clipSize = 30;
+                int bulletsInClip = default;
+                if (value <= clipSize)
+                {
+                    bulletsInClip = value;
+                    _bulletNumber = bulletsInClip;
+                }
+                else
+                {
+                    Console.WriteLine("HEEY MAN, HOLD ON! THAT'S TOO MANY BULLETS FOR THE CLIP. REDUCE THE NUMBER :D");
+                    _bulletNumber = value;
+                }
+                if (_bulletNumber > 30)
+                    return;
+                else
+                {
+                    if (_shootMode == "Single".ToLower())
+                    {
+                        while (_bulletNumber > 0)
+                        {
+                            _bulletNumber--;
+                            if (_bulletNumber == 0) { Console.WriteLine($"BANG!! Nice single shot.Refill the gun"); return; }
+                            Console.WriteLine($"BANG!! Nice single shot. Still you've {_bulletNumber}");
+                        }
+                    }
+                    else
+                    {
+                        int multiShoot = 3;
+                        if (_bulletNumber == 1 || _bulletNumber == 2)
+                        {
+                            Console.WriteLine("Please add more bullets for multi shoot");
+                            return;
+                        }
+                        while (_bulletNumber > 0)
+                        {
+                            _bulletNumber -= multiShoot;
+                            if (_bulletNumber <= 0 || _bulletNumber < 3) { Console.WriteLine($"BANG!! Nice multi shot.Refill the gun"); return; }
+                            Console.WriteLine($"BANG!! Nice multi shot. Still you've {_bulletNumber}");
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Access-Modifiers-Part2-Dll-Namespace/Program.cs
@@ -0,0 +1,91 @@
+﻿using Access_Modifiers_Part2_Dll_Namespace.Models;
+using System.Text.RegularExpressions;
+#region Task-1
+//Regex ve Math tiplerini arashdirib example yazin(hansi methodlari var ve.s)
+#region Regex
+/*//Matching a Pattern
+string input = "The cat chased the mouse around the house.";
+string pattern = @"\b[a-z]{5}\b";
+MatchCollection matches = Regex.Matches(input, pattern);
+foreach (Match match in matches)
+{
+    Console.WriteLine($"Match found: {match.Value}");
+}*/
+/*//Replacement
+string input = "The cat chased the mouse around the house.";
+string pattern = @"\bcat\b";
+string replacement = "dog";
+string result = Regex.Replace(input, pattern, replacement);
+Console.WriteLine($"Result: {result}");*/
+/*//Splitting
+string input = "orange,apple,pear";
+string pattern = ",";
+
+string[] splitStrings = Regex.Split(input, pattern);
+
+foreach (string str in splitStrings)
+{
+    Console.WriteLine($"Fruit: {str}");
+}*/
+/*//Is Matching
+string input = "The cat chased the mouse around the house.";
+string pattern = @"\b[a-z]{5}\b";
+bool isMatch = Regex.IsMatch(input, pattern);
+if (isMatch)
+{
+    Console.WriteLine(isMatch);
+}
+else
+{
+    Console.WriteLine(isMatch);
+}*/
+/*//Matches
+string input = "The cat chased the mouse around the house.";
+string pattern = @"\b[a-z]{5}\b";
+MatchCollection matches = Regex.Matches(input, pattern);
+if (matches.Count > 0)
+{
+    Console.WriteLine("Founded:");
+    foreach (Match match in matches)
+    {
+        Console.WriteLine(match.Value);
+    }
+else
+{
+    Console.WriteLine("Nothing founded");
+}}*/
+#endregion
+#region Math
+//Mostly used:
+/*//Finding max and min ==>Math.Max(x,y); , Min(x,y);
+int x = 8;
+double y = 12.5;
+int z = 9;
+var resMax = Math.Max(x, y);
+var resMin = Math.Min(x, z);    
+var resMinOf3 = Math.Min(x, Math.Min(y,z));
+var resMaxOf3 = Math.Max(x, Math.Max(y, z));
+Console.WriteLine($"Max is {resMax}, Min is {resMin}, Max of 3 is {resMaxOf3}, Min of 3 is {resMinOf3}");*/
+/*//Finding square root of number ==> Math.Sqrt(x);
+int num = 16;
+var res = Math.Sqrt(num);
+Console.WriteLine(res);*/
+/*//Negative num turns into positive ==>Math.Abs(x);
+var num = -.6;
+Console.WriteLine(Math.Abs(num));*/
+/*//Rounds a number to the nearest whole number ==>Math.Round(x);
+var num = 45.3;
+Console.WriteLine(Math.Round(num));*/
+/*//Power of number ==>Math.Pow(x);
+int num = 5;
+Console.WriteLine(Math.Pow(num,3));*/
+#endregion
+#endregion
+#region Task-2
+//Avtomat simulyasiyası yaratmaq.(daraq,tektek ve avtomat rejimde atis,daragin doldurulmasi)
+/*Gun shooter = new();
+Console.WriteLine("Please Enter Shooting Mode : Single or Multi");
+shooter.ShooterMode = Console.ReadLine().ToLower();
+Console.WriteLine("Please Fill Clip With Bullets:");
+shooter.Bullet = int.Parse(Console.ReadLine());*/
+#endregion
